87b4a59ab1f5ed5c99468254182552982d60df8a
Enoch Ansah <enocansah@yahoo.com>
2023-09-23 02:50:53 +0000

    Increased Glow effect opacity

--- components/Glow.tsx
+++ components/Glow.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Glow = ({ className }: Props) => {
   return (
-    <div className={`absolute rounded-full opacity-40 w-44 h-44 md:w-72 md:h-72 bg-tertiary-50 blur-3xl ${className}`}></div>
+    <div className={`absolute rounded-full opacity-60 w-44 h-44 md:w-72 md:h-72 bg-tertiary-50 blur-3xl ${className}`}></div>
   );
 };
 
